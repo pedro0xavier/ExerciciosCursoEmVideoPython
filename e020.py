4c2261b02f05bc58a36125a5ae97a77fcaabe125
pedro0xavier <147566512+pedro0xavier@users.noreply.github.com>
2023-10-16 21:32:04 -0300

    Update e020.py

--- e020.py
+++ e020.py
@@ -1,4 +1,5 @@
-''' O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.'''
+''' O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. 
+Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.'''
 from random import shuffle
 n1 = str(input('Primeiro aluno: '))
 n2 = str(input('Segundo aluno: '))
