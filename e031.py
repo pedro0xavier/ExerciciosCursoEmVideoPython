218f8c1b5f089e404666ad93010152f32a2c4b85
pedro0xavier <147566512+pedro0xavier@users.noreply.github.com>
2023-10-16 21:29:49 -0300

    Update e031.py

--- e031.py
+++ e031.py
@@ -1,4 +1,5 @@
-'''Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.'''
+'''Desenvolva um programa que pergunte a distância de uma viagem em Km. 
+Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.'''
 kmviagem = float(input('Qual é a distância da viagem? '))
 print('Você está prestes a começar uma viagem de {:.1f}km'.format(kmviagem))
 custoviagem = kmviagem * 0.45 if kmviagem <= 200 else kmviagem * 0.45
