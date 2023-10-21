0ff08924e112d2de060e95fe7fc95b7ce069846e
pedro0xavier <xavierpedrojp@gmail.com>
2023-10-21 20:41:11 -0300

    Create e057.py

---
+++ e057.py
@@ -0,0 +1,6 @@
+''' Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores M ou F. Caso esteja errado, peça a digitação novamente até ter um valor correto.'''
+
+sexo = str(input('Digite o seu sexo[M/F]: ')).strip().upper()[0]
+while sexo not in 'MF':
+        sexo = str(input('Dados inválidos....Por favor insira dados certos: ')).strip().upper()[0]
+print('Dados registrados com sucesso')        
