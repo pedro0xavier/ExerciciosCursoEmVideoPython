a2d0e75ed77df543826d5b5bf089c5c7e09fb0bc
pedro0xavier <xavierpedrojp@gmail.com>
2023-11-02 17:23:53 -0300

    Update e071.py

--- e071.py
+++ e071.py
@@ -1,14 +1,21 @@
 print ('-=-' * 30)
 print ('{:^70}'.format('Banco'))
 print ('-=-' * 30)
-valor = int(input('Qual valor você quer sacar?'))
-valor50 = valor // 50
+valor = int(input('Qual valor você quer sacar?: '))
+valor100 = valor // 100
+valor50 = valor % 100 // 50
 valor20 = (valor % 50) // 20
 valor10 = ((valor % 50)% 20) // 10
 valor1 = valor % 10
 print (f'Valor R${valor} ')
-print (F'Em notas de 50: {valor50}')
-print (F'Em notas de 20: {valor20}')
-print (f'Em notas de 10: {valor10}')
-print (f'Em notas de 1: {valor1}')
+if valor100 > 0 or valor100 > valor:
+    print (f'Em notas de 100: {valor100}')
+if valor50 > 0:
+    print (F'Em notas de 50: {valor50}')
+if valor20 > 0:
+    print (F'Em notas de 20: {valor20}')
+if valor10 > 0:
+    print (f'Em notas de 10: {valor10}')
+if valor1 > 0:
+    print (f'Em notas de 1: {valor1}')
 
