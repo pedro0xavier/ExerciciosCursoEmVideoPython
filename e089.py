7cafc1387360628ae01dd70c0d0fe65fb8cf4b5d
pedro0xavier <xavierpedrojp@gmail.com>
2023-11-16 22:03:14 -0300

    exercícios

--- e089.py
+++ e089.py
@@ -1,24 +1,27 @@
-'''Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.'''
 dados = list()
-pessoa = list()
-media = list()
+
 while True:
-    dados.append(str(input('Nome:' )))
-    dados.append(float(input(f'Nota 1: ')))
-    dados.append(float(input(f'Nota 2: ')))
-    for i in range(0,2):
-        media.append(dados[i][:])
-        
-    pessoa.append(dados[:])
-    dados.clear()
-    resp = str(input('Quer continuar?: [S/N]')).strip().upper()[0]
-    if resp in 'N':
+    nome = str(input('Nome: ').title()) 
+    n1 = float(input('Nota 1: '))
+    n2 = float(input('Nota 2: '))
+    media = (n1 + n2) / 2
+    dados.append([nome, [n1, n2], media])
+    
+    resp = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+    if resp == 'N':
         break
-print('=-='*30)
-print(f'No.',end=' ')
-print('Nome',end='')
-print(f'{"Média": >20}')
-for i in range(len(pessoa)):
-    print(i,end='  ')
-    print(f'{pessoa[i][0]}',end='  ')
-    print()
+
+print('=-=' * 15)
+print(f'{"No.": <4}{"Nome": <10}{"Média": >8}')
+print('-' * 30)
+
+for i, aluno in enumerate(dados, 1):
+    print(f'{i:<4}{aluno[0]:<10}{aluno[2]:>8.1f}')
+
+while True:
+    index = int(input('\nMostrar a nota de qual aluno (baseado no número da chamada) [Digite 999 para sair]: ')) - 1
+
+    if index == 999:
+        break
+
+    print(f'\nNotas do(a) {dados[index][0]}: {dados[index][1]}')
