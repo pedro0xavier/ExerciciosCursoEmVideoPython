9b0756742654ce3d5136e9dee96edf2f912cd496
pedro0xavier <xavierpedrojp@gmail.com>
2023-11-24 01:51:34 -0300

    exercícios

--- e093.py
+++ e093.py
@@ -12,9 +12,10 @@ print (dados)
 print('-=-' * 30)
 for keyy,itemm in dados.items():
     print(f'{keyy} é {itemm}')
+print('-=-' * 30)
 print(f'O jogador {dados["Nome"]} jogou {games} jogos ')
-for i in range (0,games):
-    print(f'Na partida {i + 1} marcou {gols[i]} gols')
+for i,g in enumerate(dados['Gols']):
+    print(f'Na partida {i + 1} marcou {g} gols')
 print(f'Foi um total de {dados["Total"]} gols')
 print(f'A partida que mais marcou gols foi na {gols.index(max(gols)) + 1}° onde marcou {max(gols)} gols')
 print(f'A partida que menos marcou gols foi na {gols.index(min(gols)) + 1}° onde marcou {min(gols)} gols')
