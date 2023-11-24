e402761ec835249751696a213149f7c47018720a
pedro0xavier <xavierpedrojp@gmail.com>
2023-11-24 02:11:07 -0300

    exercícios

--- e093.py
+++ e093.py
@@ -1,3 +1,4 @@
+'''Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.'''
 dados = {}
 gols = list()
 dados['Nome'] = str(input('Nome do jogador: ')).strip().title()
