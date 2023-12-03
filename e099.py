66692aefba458c55eea33ec5d9aed82424a97c03
pedro0xavier <xavierpedrojp@gmail.com>
2023-12-03 02:08:46 -0300

    exercícios

--- e099.py
+++ e099.py
@@ -1,8 +1,10 @@
 ''' Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.'''
 def maior(*num):
     bigger = max(num,key=int)
-    for _,numeros in enumerate(num):
-        print(f'Foi digitado o número {numeros} ')
+    print(f'Foram printados os números:',end=' ')
+    for numeros in num:
+        print(f'{numeros}',end=' ')
+    print()
     print(f'Ao todo foram digitados {len(num)} números ')
     print(f'O maior número digitado foi {bigger}')
 maior(5,4,8,9,3)
