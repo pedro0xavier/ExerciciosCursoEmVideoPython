1284566d11abd08aa71008ebf10e0eb964c1c3d6
pedro0xavier <xavierpedrojp@gmail.com>
2023-12-04 18:46:18 -0300

    exercício

--- e102.py
+++ e102.py
@@ -1,14 +1,14 @@
 def fatorial(num,show):
     fact = 1
     c = 0
-    for i in range(num,0,-1):
-        fact *= i
-    if show:
-        print(f'{num}',end='')
-        for c in range(num - 1, 0,-1):
-            print(f' X {c}',end='')
+    for c in range(num , 0,-1):
+        if show:
+            if c >= 1:
+                print(f'{c} ',end='')
+            if c > 1:
+                print('X',end=' ')
+            fact*=c
             if c == 1:
                 print(f' =  {fact}')
     return fact
-print(fatorial(9,show = True))
 print(fatorial(5,show = True))
