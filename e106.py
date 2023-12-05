4c6d8cb82ae2a2f97d18f231d5aad3dd5434948e
pedro0xavier <xavierpedrojp@gmail.com>
2023-12-05 20:10:30 -0300

    exercício

--- e106.py
+++ e106.py
@@ -4,13 +4,14 @@ def ajuda(ajuuda):
     print('-=-' * 30)
     help(ajuuda)
 
+
 def titulo(ajuuda):
     tam = len(ajuuda)
     print('-=-' * tam)
     print(ajuuda)
     print('-=-' * tam)
 
-    
+
 while True:
     titulo("SISTEMA DE AJUDA PYHELP")
     comando = str(input('Digite a função ou biblioteca: ')).strip()
