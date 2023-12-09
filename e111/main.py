8d22435db776e840e89587d3c0b01fff3c3eff15
pedro0xavier <xavierpedrojp@gmail.com>
2023-12-09 20:50:29 -0300

    exercícios

--- e111/main.py
+++ e111/main.py
@@ -1,6 +1,7 @@
 from utilidadesCeV import moeda
+from utilidadesCeV import dado
 '''Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado. Transfira todas as funções utilizadas nos desafios 107, 108 e 109 para o primeiro pacote e mantenha tudo funcionando'''
-valor = float(input('Digite o preço: R$'))
-Aumento = float(input('Taxa de aumento: '))
-Reducao = float(input('Taxa de redução: '))
+valor = dado.leiadinheiro('Preço: ')
+Aumento = dado.leiadinheiro('Taxa de Aumento: ')
+Reducao = dado.leiadinheiro('Taxa de Diminuindo: ')
 moeda.resumo(valor,Aumento,Reducao)
---
+++ e111/utilidadesCeV/dado/__init__.py
@@ -0,0 +1,17 @@
+'''Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado. Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função imputa(), mas com uma validação de dados para aceitar apenas valores que seja monetários.'''
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+def leiadinheiro(msg):
+    valido = False
+    while not valido:
+        entrada =str(input(msg)).replace(',','.')
+        if entrada.isnumeric() or is_number(entrada) == True:
+            valido = True
+            return float(entrada)
+        else:
+            print(f'ERRO {entrada} é um preço inválido')
