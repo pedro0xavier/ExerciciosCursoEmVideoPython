5ce3b24c403370f87818fd96b2f971db62a73ff0
pedro0xavier <xavierpedrojp@gmail.com>
2023-12-09 19:06:39 -0300

    exercícios

---
+++ e110/teste.py
@@ -0,0 +1,6 @@
+from moeda import * 
+'''Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.'''
+valor = float(input('Digite o preço: R$'))
+Aumento = float(input('Taxa de aumento: '))
+Reducao = float(input('Taxa de redução: '))
+resumo(valor,Aumento,Reducao)
