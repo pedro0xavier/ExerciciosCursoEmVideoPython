5f8acb66281484c9b430370d786ca5bb5fad4dad
pedro0xavier <xavierpedrojp@gmail.com>
2024-02-08 18:46:28 -0300

    exercícios

--- e106.py
+++ e106.py
@@ -1,3 +1,4 @@
+'''Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra FIM , o programa se encerrará. Importante: use cores.'''
 def ajuda(ajuuda):
     print('-=-'* 30)
     print(f'Acessando o comando {ajuuda}')
